a6c6d0008d67951d17ca64bc5c34fcb356c83286
KonstBeliakov <konstbeliakov@gmail.com>
2025-10-19 22:21:21 +0300

    added file 1_2.py in graph_decomposition2

---
+++ graph_decomposition2/1_2.py
@@ -0,0 +1,55 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+
+n, m = map(int, input().split())
+edges = [[int(i) for i in input().split()] for _ in range(m)]
+adj = [[] for _ in range(n + 1)]
+adj_r = [[] for _ in range(n + 1)]
+for edge in edges:
+    adj[edge[0]].append(edge[1])
+    adj_r[edge[1]].append(edge[0])
+
+adj_r_visited = [False] * (n + 1)
+visit_order = []
+
+
+def dfs_postorder(adj, vertex):
+    global visit_order
+    adj_r_visited[vertex] = True
+    for v in adj[vertex]:
+        if not adj_r_visited[v]:
+            dfs_postorder(adj, v)
+    visit_order.append(vertex)
+
+
+def dfs_mark_components(adj, vertex):
+    global current_component_number, last_component_size  # , component_dict
+    component[vertex] = current_component_number
+    # component_dict[current_component_number] = component_dict.get(current_component_number, []) + [vertex]
+    last_component_size += 1
+    for v in adj[vertex]:
+        if component[v] is None:
+            dfs_mark_components(adj, v)
+
+
+for v in range(1, n + 1):
+    if not adj_r_visited[v]:
+        dfs_postorder(adj_r, v)
+
+component = [None] * (n + 1)
+# component_dict = {}
+current_component_number = 0
+last_component_size = 0
+for v in reversed(visit_order):
+    if component[v] is None:
+        current_component_number += 1
+        last_component_size = 0
+        dfs_mark_components(adj, v)
+        if last_component_size > 1:
+            print(1)
+            break
+else:
+    print(0)
+
+# print(component_dict)
